2b39f1ce6221661d83af24c912c90f1cfa8a8d1b
patricktiamson <tiamson.patrick@outlook.com>
2020-01-17 09:21:12 -0500

    fixed odometry stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/external/AdvancedMath/WrapperHandler.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/external/AdvancedMath/WrapperHandler.kt
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.utilities.external.AdvancedMath
 
 class WrapperHandler(private val max: Double, val function: () -> Double) {
+
     var finalValue: Double = 0.0
+
     private var prev = finalValue
     private fun getDelta(curr: Double): Double {
         return prev.closestAngleDifference(curr)
@@ -11,6 +13,7 @@ class WrapperHandler(private val max: Double, val function: () -> Double) {
     fun getValue(): Double {
         if(firstRun){
             prev = function()
+            finalValue = prev
             firstRun=false
             return prev
         }
