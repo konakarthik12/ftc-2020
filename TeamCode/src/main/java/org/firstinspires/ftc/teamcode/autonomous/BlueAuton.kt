55ef9d96b632ca8ba7770fc0b17a1296728074d0
patricktiamson <tiamson.patrick@outlook.com>
2020-02-27 15:57:11 -0500

    auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueAuton.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueAuton.kt
@@ -23,100 +23,71 @@ class BlueAuton : MOEAuton() {
         telemetry.addData("location", location)
         telemetry.addData("gyro", robot.gyro.angle)
         telemetry.update()
-        //FIRST MOVEMENT TOWRADS STONE
+        //FROM WALL TO STONES
         robot.chassis.encoders.moveBackwardInches(28.0)
-
         robot.chassis.turnTo(90.0)
-        val verticalDistance = when (location) {
-            LEFT -> 1.5
-            MIDDLE -> -1.75
-            RIGHT -> -9.5
-        }
-        telemetry.addData("distance", verticalDistance)
-        telemetry.update()
-        robot.chassis.encoders.moveVertical(verticalDistance)
-        val strafeDistance = when (location) {
-            LEFT -> 5.5
-            MIDDLE -> 3.0
-            RIGHT -> 3.5
-        }
-        robot.chassis.turnTo(90.0)
-        robot.chassis.encoders.moveRightInches(strafeDistance)
-        wait(500)
+        //GRAB 1ST SKYSTONE
+        moveSkyStoneVertical(location,1.5,-1.75,-9.5)
+        wait(300)
+        moveSkyStoneRight(location, 5.5,3.0,3.5)
         robot.autonArms.right.grabStone()
         robot.autonArms.right.liftStone()
-        val strafeOffDistance = when (location) {
-            LEFT -> 5.0
-            MIDDLE -> 6.0
-            RIGHT -> 4.0
-        }
-        robot.chassis.encoders.moveLeftInches(strafeDistance + strafeOffDistance)
-        val toFoundationDistance = when (location) {
-            LEFT -> 66.5
-            MIDDLE -> 80.0
-            RIGHT -> 88.0
-        }
-        robot.chassis.encoders.moveVertical(toFoundationDistance, 0.8)
+        moveSkyStoneLeft(location, 11.5, 9.0, 7.5)
         robot.chassis.turnTo(90.0)
-        val toFoundationDistanceStrafe = when (location) {
-            LEFT -> 5.5
-            MIDDLE -> 9.5
-            RIGHT -> 9.0
-        }
-        robot.chassis.encoders.moveRightInches(toFoundationDistanceStrafe)
-        robot.chassis.turnTo(90.0)
+        //PLACE 1ST STONE ON FOUNDATION
+        moveSkyStoneVertical(location, 66.5,80.0,88.0)
+        wait(300)
+        moveSkyStoneRight(location, 5.5,9.5,9.0)
         robot.autonArms.right.dropStone()
         robot.autonArms.right.raiseArm()
         robot.autonArms.right.initAutonArm()
-        //wait(1000)
-        val toFoundationDistanceStrafeOff = when (location) {
-            LEFT -> 3.0
-            MIDDLE -> 1.5
-            RIGHT -> 1.5
-        }
-        robot.chassis.encoders.moveLeftInches(toFoundationDistanceStrafe+toFoundationDistanceStrafeOff)
         robot.chassis.turnTo(90.0)
-
-        val firstBackwards = when (location) {
-            LEFT -> 100.0
-            MIDDLE -> 110.0
-            RIGHT -> 118.0
-        }
-        robot.chassis.encoders.moveBackwardInches(firstBackwards)
+        moveSkyStoneLeft(location, 8.5,11.0,10.5)
+        //GRAB 2ND SKYSTONE
         robot.chassis.turnTo(90.0)
-        val rightStrafe = when (location) {
-            LEFT -> 4.5
-            MIDDLE -> 5.5
-            RIGHT -> 9.0
-        }
-        robot.chassis.encoders.moveRightInches(rightStrafe)
+        moveSkyStoneVertical(location, -100.0,-110.0,118.0)
+        wait(300)
+        moveSkyStoneRight(location, 4.5,5.5,9.0)
         robot.autonArms.right.grabStone()
         robot.autonArms.right.liftStone()
-        val leftStrafeOff = when (location) {
-            LEFT -> 0.0
-            MIDDLE -> 0.0
-            RIGHT -> 0.0
-        }
-        robot.chassis.encoders.moveLeftInches(rightStrafe + leftStrafeOff)
-        val toFoundationDistance2 = when (location) {
-            LEFT -> 100.0
-            MIDDLE -> 110.0
-            RIGHT -> 118.0
-        }
-        robot.chassis.encoders.moveVertical(toFoundationDistance2, 0.8)
+        moveSkyStoneLeft(location, 4.5,5.5,9.0)
         robot.chassis.turnTo(90.0)
-        val toFoundationDistanceStrafe2 = when (location) {
-            LEFT -> 6.5
-            MIDDLE -> 9.5
-            RIGHT -> 9.0
-        }
-        robot.chassis.encoders.moveRightInches(toFoundationDistanceStrafe2)
-        robot.chassis.turnTo(90.0)
+        //PLACE 2ND STONE ON FOUNDATION
+        moveSkyStoneVertical(location, 108.0,118.0,126.0)
+        wait(300)
+        moveSkyStoneRight(location, 6.5,9.5,9.0)
         robot.autonArms.right.dropStone()
         robot.autonArms.right.raiseArm()
         robot.autonArms.right.initAutonArm()
+        robot.chassis.turnTo(90.0)
+        moveSkyStoneLeft(location, 6.5, 9.5, 9.0)
+        robot.chassis.turnTo(90.0)
+        //PARK
+        moveSkyStoneVertical(location, -20.0,-20.0,-20.0)
 
         robot.chassis.stop()
+    }
+
+    private fun moveSkyStoneVertical(location: SkyStoneLocation, leftD: Double, middleD: Double, rightD: Double){
+        robot.chassis.encoders.moveVertical(when (location){
+            LEFT -> leftD
+            MIDDLE -> middleD
+            RIGHT -> rightD
+        })
+    }
+    private fun moveSkyStoneRight(location: SkyStoneLocation, leftD: Double, middleD: Double, rightD: Double){
+        robot.chassis.encoders.moveRightInches(when (location){
+            LEFT -> leftD
+            MIDDLE -> middleD
+            RIGHT -> rightD
+        })
+    }
+    private fun moveSkyStoneLeft(location: SkyStoneLocation, leftD: Double, middleD: Double, rightD: Double){
+        robot.chassis.encoders.moveLeftInches(when (location){
+            LEFT -> leftD
+            MIDDLE -> middleD
+            RIGHT -> rightD
+        })
     }
 
     private fun getSkyStoneLocation(): SkyStoneLocation {
