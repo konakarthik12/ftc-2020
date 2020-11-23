75e2bc7c24f78000cfef55e06ec5c02663f10fad
greenpizza1203 <konakarthik12@gmail.com>
2020-11-23 15:24:21 -0500

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/UltimateGoalTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/UltimateGoalTeleOp.kt
@@ -23,31 +23,6 @@ open class UltimateGoalTeleOp : MOETeleOp() {
             it.setFromPolar(polar, gpad1.right.stick.x())
         }
 
-//        val scaleX = 1.0
-//        val scaleY = 1.0
-//        val scaleRot = 0.75
-//
-//        val angle = robot.gyro.angle
-//
-//        var rawY = gpad1.left.stick.y()
-//        var rawX = gpad1.left.stick.x()
-//        var rot = gpad1.right.stick.x()
-//
-//        var throttle = powerRange.lerp(gpad1.left.trigger())
-//        if (gpad1.right.trigger() > 0.1) throttle = 0.4
-//        rawX *= scaleX * throttle
-//        rawY *= scaleY * throttle
-//        rot *= scaleRot * throttle
-//
-//        if (gpad1.left.bumper() && abs(rot) > 0.1) {
-//            rawY = rawY.coerceAtLeast(0.0)
-//        }
-//
-//        val angRad = Math.toRadians(angle)
-//        val fwd = rawX * sin(angRad) + rawY * cos(angRad)
-//        val str = rawX * cos(angRad) - rawY * sin(angRad)
-//
-//        robot.chassis.setPower(Powers.fromMechanum(fwd, str, rot, maxPower))
     }
 
 
@@ -60,14 +35,6 @@ open class UltimateGoalTeleOp : MOETeleOp() {
 
     open fun log() {
         telemetry.addData("Running", this::class.simpleName)
-//        val powers = this.robot.chassis.getPowers()
-//        telemetry.addData("powers", powers.map { it.toFixed(3) })
-//        val up = "\u2191"
-//        val middle = " "
-//        val down = "\u2193"
-//        val arrows = powers.map { if (it > 0.0) up else if (it < 0.0) down else middle }
-//        telemetry.addData("front", "${arrows[0]}\t${arrows[1]}")
-//        telemetry.addData("backs", "${arrows[2]}\t${arrows[3]}")
     }
 
 
